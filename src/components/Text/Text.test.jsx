8e6abc5738d16914beb4c28fba675a7048c648ba
Vladislav Chervyakov <chervyakovve@mail.ru>
2024-06-09 12:13:53 +0700

    feat/Title, Text element ui tests

---
+++ src/components/Text/Text.test.jsx
@@ -0,0 +1,43 @@
+import { render, screen } from '@testing-library/react';
+
+import { Text } from './Text';
+
+describe('Title component', () => {
+  const text = 'i am text children';
+
+  it('должен рендерить заголовок с элементом children внутри', () => {
+    render(<Text>{text}</Text>);
+
+    expect(screen.getByText(text)).toBeInTheDocument();
+  });
+
+  it.each([
+    { className: 'one' },
+    { className: 'two' },
+    { className: 'three' }
+  ])('должен рендерить текст с заданным классом $className', ({ className }) => {
+    render(<Text className={className}>{text}</Text>);
+
+    const element = screen.getByText(text);
+
+    expect(element).toBeInTheDocument();
+    expect(element).toHaveClass(className);
+    expect(element).toHaveClass('text');
+  });
+
+  it('если isError класс error добавлися с элемент', () => {
+    render(<Text isError={true}>{text}</Text>);
+
+    const element = screen.getByText(text);
+
+    expect(element).toHaveClass('error');
+  });
+
+  it('если isSuccess класс success добавлися с элемент', () => {
+    render(<Text isSuccess={true}>{text}</Text>);
+
+    const element = screen.getByText(text);
+
+    expect(element).toHaveClass('success');
+  });
+});
